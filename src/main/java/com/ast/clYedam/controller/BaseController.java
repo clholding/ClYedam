f02a5de6d803051c1a13c16207f493b4347889ac
CGJ <qwertyu5258@naver.com>
2023-05-14 17:16:25 +0900

    [개발] 회원가입, 로그인 부분

--- src/main/java/com/ast/clYedam/controller/BaseController.java
+++ src/main/java/com/ast/clYedam/controller/BaseController.java
@@ -1,8 +1,12 @@
 package com.ast.clYedam.controller;
 
+import com.ast.clYedam.A1Member.vo.UserVo;
 import com.ast.clYedam.service.UserService;
 import com.ast.clYedam.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -30,10 +34,35 @@ public class BaseController {
     public String index() {
         return "index";
     }
-
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
     @RequestMapping("/signUp")
     public String signUp() {
         return "signUp";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @PostMapping("/loginSubmit")
+    public String loginForm(@RequestParam("username") String username, @RequestParam("password") String password) {
+        if (authenticate(username, password)) {
+            // 인증 성공한 경우
+            return "redirect:/index";
+        } else {
+            // 인증 실패한 경우
+            return "redirect:/login?error";
+        }
+    }
+    // 사용자 인증을 수행하는 메서드
+    private boolean authenticate(String username, String password) {
+        // 인증 로직 구현
+        // 실제로는 데이터베이스나 외부 시스템과의 연동 등을 통해 사용자 인증을 수행해야 합니다.
+        // 여기에서는 간단한 예시로 "user"라는 사용자와 "password"라는 비밀번호를 허용하도록 처리하겠습니다.
+        return username.equals("1") && password.equals("1");
     }
 
 }
